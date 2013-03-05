1bd1c18e878fc6db08c15ccc2f2690bb2e0210fd
Alex Auritt <alexauritt@yahoo.com>
2013-03-05 10:15:07 -0500

    empty table view

---
+++ Homepwner/ItemsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.h
+//  Homepwner
+//
+//  Created by Alexander Auritt on 3/5/13.
+//  Copyright (c) 2013 Alexander Auritt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ItemsViewController : UITableViewController
+
+@end
---
+++ Homepwner/ItemsViewController.m
@@ -0,0 +1,26 @@
+//
+//  ItemsViewController.m
+//  Homepwner
+//
+//  Created by Alexander Auritt on 3/5/13.
+//  Copyright (c) 2013 Alexander Auritt. All rights reserved.
+//
+
+#import "ItemsViewController.h"
+
+@implementation ItemsViewController
+
+- (id)init
+{
+    self = [super initWithStyle:UITableViewStyleGrouped];
+    if (self) {
+        
+    }
+    return self;
+}
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    return [self init];
+}
+@end
