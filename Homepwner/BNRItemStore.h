6fc297c452dfeb6181bd31f2a8fdda6c412b714b
Alex Auritt <alexauritt@yahoo.com>
2013-03-05 11:20:09 -0500

    p196

---
+++ Homepwner/BNRItemStore.h
@@ -0,0 +1,22 @@
+//
+//  BNRItemStore.h
+//  Homepwner
+//
+//  Created by Alexander Auritt on 3/5/13.
+//  Copyright (c) 2013 Alexander Auritt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class BNRItem;
+
+@interface BNRItemStore : NSObject
+{
+    NSMutableArray *allItems;
+}
+
++ (BNRItemStore *)sharedStore;
+
+- (NSArray *)allItems;
+- (BNRItem *)createItem;
+@end
---
+++ Homepwner/BNRItemStore.m
@@ -0,0 +1,48 @@
+//
+//  BNRItemStore.m
+//  Homepwner
+//
+//  Created by Alexander Auritt on 3/5/13.
+//  Copyright (c) 2013 Alexander Auritt. All rights reserved.
+//
+
+#import "BNRItemStore.h"
+#import "BNRItem.h"
+
+@implementation BNRItemStore
+
++ (BNRItemStore *)sharedStore
+{
+    static BNRItemStore *sharedStore = nil;
+    if (!sharedStore)
+        sharedStore = [[super allocWithZone:nil] init];
+    
+    return sharedStore;
+}
+
++ (id)allocWithZone:(NSZone *)zone
+{
+    return [self sharedStore];
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        allItems = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
+- (NSArray *)allItems
+{
+    return allItems;
+}
+
+- (BNRItem *)createItem
+{
+    BNRItem *p = [BNRItem randomItem];
+    [allItems addObject:p];
+    return p;
+}
+@end
