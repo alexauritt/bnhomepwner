4cd4dfb3b777692f3ac0a349d96766d8067ac788
Alex Auritt <alexauritt@yahoo.com>
2013-03-23 16:47:27 -0400

    ChangeDateViewController

---
+++ Homepwner/ChangeDateViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChangeDateViewController.h
+//  Homepwner
+//
+//  Created by Alexander Auritt on 3/14/13.
+//  Copyright (c) 2013 Alexander Auritt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChangeDateViewController : UIViewController
+
+@end
---
+++ Homepwner/ChangeDateViewController.m
@@ -0,0 +1,38 @@
+//
+//  ChangeDateViewController.m
+//  Homepwner
+//
+//  Created by Alexander Auritt on 3/14/13.
+//  Copyright (c) 2013 Alexander Auritt. All rights reserved.
+//
+
+#import "ChangeDateViewController.h"
+
+@interface ChangeDateViewController ()
+
+@end
+
+@implementation ChangeDateViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
